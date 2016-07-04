61f710689ebeddfc5ad656c7c69c5ba5498df593
Divya <divyamounica@chargeback360.com>
2016-07-04 12:35:12 +0530

    add psot for todos

--- server1.js
+++ server1.js
@@ -1,44 +1,49 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [];
+var todoNextId = 1;
 
-var todos = [{
-	id: 1,
-	description: 'meet mom for lunch',
-	completed: false
-},{
-	id: 2,
-	descrption: 'in a meeting',
-	completed: false
-},{
-	id: 3,
-	description: 'got it',
-	completed: true
-}];
-app.get('/', function(req, res) {
-	res.send('todo API root' );
+app.use(bodyParser.json());
+app.get('/', function (req, res) {
+	res.send('Todo API Root');
 });
 
-
-app.get('/todos', function (req,res) {
+// GET /todos
+app.get('/todos', function (req, res) {
 	res.json(todos);
-})
-app.get('/todos/:id', function (req,res) {
+});
+
+// GET /todos/:id
+app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo;
+
 	todos.forEach(function (todo) {
 		if (todoId === todo.id) {
 			matchedTodo = todo;
 		}
 	});
-	if (matchedTodo){
+
+	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
 		res.status(404).send();
 	}
-// 	res.send('asking for todo with id of' + req.params.id);
-// });
+});
+
+//post/todos
+
+app.post('/todos', function(req, res) {
+	var body= req.body;
+
+	body.id =todoNextId++;
+	todos.push(body);
+	// console.log('descrption:' + body.description);
+	res.json(body);
+})
 
 app.listen(PORT, function () {
-	console.log('express listening on port '  + PORT + '!');
+	console.log('Express listening on port ' + PORT + '!');
 });
